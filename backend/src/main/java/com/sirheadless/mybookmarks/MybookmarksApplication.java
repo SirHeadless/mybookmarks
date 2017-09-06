2b4cc2dc5374445651844bc826df1cbdb8237629
SirHeadless <krichie4014@aol.com>
2017-09-06 21:15:08 +0200

    Init commit

---
+++ backend/src/main/java/com/sirheadless/mybookmarks/MybookmarksApplication.java
@@ -0,0 +1,12 @@
+package com.sirheadless.mybookmarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybookmarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybookmarksApplication.class, args);
+	}
+}
